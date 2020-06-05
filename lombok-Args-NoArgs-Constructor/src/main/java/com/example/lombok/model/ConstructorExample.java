45c517ea70045d8636ea84bb21f2563282c30273
Ravishankarcode <54878415+Ravishankarcode@users.noreply.github.com>
2020-06-06 01:32:19 +0530

    comment added

--- lombok-Args-NoArgs-Constructor/src/main/java/com/example/lombok/model/ConstructorExample.java
+++ lombok-Args-NoArgs-Constructor/src/main/java/com/example/lombok/model/ConstructorExample.java
@@ -9,6 +9,11 @@ import lombok.ToString;
  * 
  * @author Ravishankar.kumar
  * @RequiredArgsConstructor generates a constructor with 1 parameter for each field that requires special handling.
+ * 
+ * @RequiredArgsConstructor(staticName = "of") same as below code
+ *  public static ConstructorExample of(String description) {
+ *  return new ConstructorExample(description);
+ *  }
  */
 @RequiredArgsConstructor(staticName = "of")
 @AllArgsConstructor(access = AccessLevel.PUBLIC)
